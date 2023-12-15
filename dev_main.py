fbbb60911b798043c1511ed6d918a76c97ee2550
Christopher Davies <chris.davies@weavermanor.co.uk>
2023-12-15 16:18:10 +0000

    Testing code additions Still in development

--- dev_main.py
+++ dev_main.py
@@ -14,6 +14,8 @@ class Device(asyncssh.client.SSHClient):
         self.hostname = None
         self.serial_numbers = None
         self.uptime = None
+        self.cdp_neighbour_information = None
+        self.version_information = None
         self.encryption_algs_list = ["aes128-cbc", "3des-cbc", "aes192-cbc", "aes256-cbc", "aes256-ctr"]
         self.kex_algs_list = ["diffie-hellman-group-exchange-sha1", "diffie-hellman-group14-sha1",
                               "diffie-hellman-group1-sha1"]
@@ -35,30 +37,36 @@ class Device(asyncssh.client.SSHClient):
         with open(f"textfsm/cisco_ios_show_cdp_neighbors_detail.textfsm") as f:
             re_table = textfsm.TextFSM(f)
             output = re_table.ParseText(show_cdp_neighbours.stdout)
-        get_cdp_nei_parsed = [dict(zip(re_table.header, entry)) for entry in output]
+        self.cdp_neighbour_information = [dict(zip(re_table.header, entry)) for entry in output]
 
-        get_cdp_nei_parsed[0]["LOCAL_IP"] = self.ipaddr
-        get_cdp_nei_parsed[0]["LOCAL_HOST"] = self.hostname
-        get_cdp_nei_parsed[0]["LOCAL_SERIAL"] = self.serial_numbers
-        get_cdp_nei_parsed[0]["LOCAL_UPTIME"] = self.uptime
-        dest_host = get_cdp_nei_parsed[0]['DESTINATION_HOST']
+        self.cdp_neighbour_information[0]["LOCAL_IP"] = self.ipaddr
+        self.cdp_neighbour_information[0]["LOCAL_HOST"] = self.hostname
+        self.cdp_neighbour_information[0]["LOCAL_SERIAL"] = self.serial_numbers
+        self.cdp_neighbour_information[0]["LOCAL_UPTIME"] = self.uptime
+        dest_host = self.cdp_neighbour_information[0]['DESTINATION_HOST']
         head, sep, tail = dest_host.partition('.')
-        get_cdp_nei_parsed[0]['DESTINATION_HOST'] = head.upper()
+        self.cdp_neighbour_information[0]['DESTINATION_HOST'] = head.upper()
+        await self.close()
 
-        return get_cdp_nei_parsed[0]
+        return self.cdp_neighbour_information[0]
 
     async def get_version(self):
         show_version = await self.connection.run('show version')
         with open(f"textfsm/cisco_ios_show_version.textfsm") as f:
             re_table = textfsm.TextFSM(f)
             output = re_table.ParseText(show_version.stdout)
-        get_version_results_parsed = [dict(zip(re_table.header, entry)) for entry in output]
+        self.version_information = [dict(zip(re_table.header, entry)) for entry in output]
 
-        self.hostname = get_version_results_parsed[0].get("HOSTNAME")
-        self.serial_numbers = get_version_results_parsed[0].get("SERIAL")
-        self.uptime = get_version_results_parsed[0].get("UPTIME")
+        self.hostname = self.version_information[0].get("HOSTNAME")
+        self.serial_numbers = self.version_information[0].get("SERIAL")
+        self.uptime = self.version_information[0].get("UPTIME")
+        await self.close()
 
-        return get_version_results_parsed[0]
+        return self.version_information[0]
+
+    def get_info(self, string):
+        device_information = {**self.cdp_neighbour_information[0], **self.version_information[0]}
+        return device_information[string]
 
     async def close(self):
         self.connection.close()
@@ -71,14 +79,16 @@ async def main():
 
     await device.connect()
     cdp_output = await device.get_cdp_neighbors()
-    await device.close()
 
     await device.connect()
     version_output = await device.get_version()
-    await device.close()
 
     merged = {**cdp_output, **version_output}
     print(merged)
+    print(device.get_info("MANAGEMENT_IP"))
+
+    if device:
+        print("")
 
 
 asyncio.run(main())
